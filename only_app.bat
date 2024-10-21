46184dbe2888d18637344b927ff74453eae4e4b2
Shadow403 <admin@shadow403.cn>
2024-10-21 14:02:30 +0800

    :arrow_up: update .bat title version

--- only_app.bat
+++ only_app.bat
@@ -1,2 +1,2 @@
 @echo off
-start cmd /k "title FastAPI v0.1.0 && .\.venv\Scripts\activate && python app.py && pause"
+start cmd /k "title FastAPI v0.1.3 && .\.venv\Scripts\activate && python app.py && pause"
