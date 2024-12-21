f8407a40e8a4be59d834eb1075efa565a185cab4
Shadow403 <admin@shadow403.cn>
2024-12-21 14:59:30 +0800

    :art: fixed cmd outputs

--- app/pusher.py
+++ app/pusher.py
@@ -1,10 +1,13 @@
 import uvicorn
+from colorama import init
 
 from pusher.app import app
 from config import config, ASCII_LOGO
 from config.utils import init_config
 from database.connector import config_database_init
 
+
+init(autoreset=True)
 
 def main():
     print(ASCII_LOGO.pusher)
