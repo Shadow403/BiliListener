9bd2dfc4ed95e1a6740299c4d985acfb6a95105d
Shadow403 <admin@shadow403.cn>
2025-01-05 01:08:31 +0800

    :sparkles: disable choose on console

--- app/pusher.py
+++ app/pusher.py
@@ -1,3 +1,4 @@
+import ctypes
 import uvicorn
 from colorama import init
 
@@ -8,6 +9,9 @@ from database.connector import config_database_init
 
 
 init(autoreset=True)
+
+kernel32 = ctypes.windll.kernel32
+kernel32.SetConsoleMode(kernel32.GetStdHandle(-10), (0x4|0x80|0x20|0x2|0x10|0x1|0x00|0x100))
 
 def main():
     print(ASCII_LOGO.pusher)
