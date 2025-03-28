9b70c4813a7d86842b14bf77017f10fb447f19eb
Shadow403 <admin@shadow403.cn>
2025-03-28 16:20:35 +0800

    :bug: fixed build script

--- app/scripts/build.py
+++ app/scripts/build.py
@@ -9,4 +9,9 @@ ex = get_type()
 def build_main():
     _build_ico = f"-i {__ico_path__}listener.ico"
     _bulid_name_ = f"-n pusher-{__platform__}-{__version__}-{ex}"
-    os.system(f"pyinstaller {_build_ico} --onefile pusher.py {_bulid_name_} --clean")
+
+    _hi_curl_cffi = "--hidden-import curl_cffi --hidden-import bilibili_api.clients.CurlCFFIClient"
+    _hi_aiohttp = "--hidden-import aiohttp --hidden-import bilibili_api.clients.AioHTTPClient"
+    _hi_httpx = "--hidden-import httpx --hidden-import bilibili_api.clients.HTTPXClient"
+    
+    os.system(f"pyinstaller {_build_ico} --onefile pusher.py {_bulid_name_} {_hi_curl_cffi} {_hi_aiohttp} {_hi_httpx} --clean")
